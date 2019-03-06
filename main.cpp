a210d2e04542b5116d0263bfe3ee5fba7d2b9c5d
Alek Kundla <kundla.9@osu.edu>
2019-03-05 19:42:31 -0500

    RPS functions adapted

--- main.cpp
+++ main.cpp
@@ -796,36 +796,6 @@ void performanceTestTwoCounts () {
     driveForInches(SKID_FIRST, 15, DRIVE_POWER / 2.0);
 }
 
-// Exploration 3
-
-void printRPSLocation() {
-    float touch_x, touch_y;
-
-    //Call this function to initialize the RPS to a course
-    RPS.InitializeTouchMenu();
-
-    //Wait for touchscreen to be pressed
-    LCD.WriteLine("Press Screen to Start");
-    while(!LCD.Touch(&touch_x, &touch_y));
-
-    LCD.Clear();
-
-   //Write initial screen info
-   LCD.WriteRC("RPS Test Program",0,0);
-   LCD.WriteRC("X Position:",2,0);
-   LCD.WriteRC("Y Position:",3,0);
-   LCD.WriteRC("   Heading:",4,0);
-
-   while (true) {
-       LCD.WriteRC(RPS.X(),2,12); //update the x coordinate
-       LCD.WriteRC(RPS.Y(),3,12); //update the y coordinate
-       LCD.WriteRC(RPS.Heading(),4,12); //update the heading
-
-       Sleep(10); //wait for a 10ms to avoid updating the screen too quickly
-   }
-    //we will never get here because of the infinite while loop
-}
-
 // EXPLORATION 2 CODE
 
 void explorationTwoShaftEncoders () {
@@ -870,6 +840,135 @@ void explorationTwoShaftEncoders () {
     LCD.WriteLine("6. Driving complete, Done. ");
 }
 
+// Prints the RPS coordinates
+void printRPSLocation() {
+    float touch_x, touch_y;
+
+    //Call this function to initialize the RPS to a course
+    RPS.InitializeTouchMenu();
+
+    //Wait for touchscreen to be pressed
+    LCD.WriteLine("Press Screen to Start");
+    while(!LCD.Touch(&touch_x, &touch_y));
+
+    LCD.Clear();
+
+   //Write initial screen info
+   LCD.WriteRC("RPS Test Program",0,0);
+   LCD.WriteRC("X Position:",2,0);
+   LCD.WriteRC("Y Position:",3,0);
+   LCD.WriteRC("   Heading:",4,0);
+
+   while (true) {
+       LCD.WriteRC(RPS.X(),2,12); //update the x coordinate
+       LCD.WriteRC(RPS.Y(),3,12); //update the y coordinate
+       LCD.WriteRC(RPS.Heading(),4,12); //update the heading
+
+       Sleep(10); //wait for a 10ms to avoid updating the screen too quickly
+   }
+    //we will never get here because of the infinite while loop
+}
+
+void check_x_plus(float x_coordinate) //using RPS while robot is in the +x direction
+{
+    double slowMotorPower = 20.0;
+
+    //check whether the robot is within an acceptable range
+    while(RPS.X() < x_coordinate - 1 || RPS.X() > x_coordinate + 1)
+    {
+        // Skids are pointing Left
+        if (RPS.Heading() > 90 && RPS.Heading() < 270) {
+            if(RPS.X() > x_coordinate)
+            {
+                //pulse the motors for one tick in the correct direction
+                driveForInches(SKID_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+            else if(RPS.X() < x_coordinate)
+            {
+                //pulse the motors for a short duration in the correct direction
+                driveForInches(WHEELS_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+        }
+        // else the skids are pointing right
+        else {
+            if(RPS.X() > x_coordinate)
+            {
+                //pulse the motors for one tick in the correct direction
+                driveForInches(WHEELS_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+            else if(RPS.X() < x_coordinate)
+            {
+                //pulse the motors for a short duration in the correct direction
+                driveForInches(SKID_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+        }
+    }
+}
+
+
+void check_y_plus(float y_coordinate) //using RPS while robot is in the +y direction
+{
+    double slowMotorPower = 20.0;
+
+    //check whether the robot is within an acceptable range
+    while(RPS.Y() < y_coordinate - 1 || RPS.Y() > y_coordinate + 1)
+    {
+        // Skids are pointing up
+        if (RPS.Heading() > 0 && RPS.Heading() < 180) {
+            if(RPS.Y() > y_coordinate)
+            {
+                //pulse the motors for one tick in the correct direction
+                driveForInches(WHEELS_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+            else if(RPS.Y() < y_coordinate)
+            {
+                //pulse the motors for a short duration in the correct direction
+                driveForInches(SKID_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+        }
+        // else the skids are pointing down
+        else {
+            if(RPS.Y() > y_coordinate)
+            {
+                //pulse the motors for one tick in the correct direction
+                driveForInches(SKID_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+            else if(RPS.Y() < y_coordinate)
+            {
+                //pulse the motors for a short duration in the correct direction
+                driveForInches(WHEELS_FIRST, INCHES_PER_TICK, slowMotorPower);
+            }
+        }
+    }
+}
+
+void check_heading(float heading) //using RPS
+{
+    // While the robot is not in the correct heading
+    while (RPS.Heading() > (heading + 1) || RPS.Heading() < (heading - 1))
+    {
+        if(RPS.Heading() > heading) {
+            // If we should turn clockwise, do that
+           if ((RPS.Heading() - heading) <= 180) {
+               turnCountsInPlace(CLOCKWISE, 1, 20);
+           }
+           // Else we should go counterclockwise
+           else {
+               turnCountsInPlace(COUNTER_CLOCKWISE, 1, 20);
+           }
+        } else if(RPS.Heading() < heading) {
+            // If we should turn counterclockwise, do that
+           if ((heading - RPS.Heading()) <= 180) {
+               turnCountsInPlace(COUNTER_CLOCKWISE, 1, 20);
+           }
+           // Else we should go counterclockwise
+           else {
+               turnCountsInPlace(CLOCKWISE, 1, 20);
+           }
+        }
+    }
+}
+
 /*
  * Main function: Calls whatever other function the robot is to run.
  */
@@ -880,7 +979,7 @@ int main(void)
     //calibrateServo();
 
     // Call desired function
-    printShaftEncoderValues();
+    printRPSLocation();
 
 
     // Just a conventional best practice
